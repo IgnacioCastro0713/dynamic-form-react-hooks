f47ebf5e0acfe536e5a1d29d9d677d37dd710588
ignacio <ignaciomenchaca666@gmail.com>
2020-04-14 22:52:06 -0500

    correction

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ function App() {
   ]);
 
   const addFields = () => {
-    const values = [...inputFields];
-    values.push({firstName: '', lastName: ''});
-    setInputFields(values);
+    setInputFields([...inputFields, {firstName: '', lastName: ''}]);
   };
 
   const removeFields = index => {
@@ -23,15 +21,15 @@ function App() {
     setInputFields(values);
   };
 
-  const inputChange = (index, event) => {// así se me ocurrio de momento tu puedes mejorarlo
-    const values = [...inputFields];
-    if (event.target.name === "firstName") {
-      values[index].firstName = event.target.value;
-    } else {
-      values[index].lastName = event.target.value;
-    }
+  const inputChange = (idx, e) => {
+    const newFields = inputFields.map((field, index) => {
+      if (idx !== index) {
+        return field
+      }
 
-    setInputFields(values);
+      return {...field, [e.target.name]: e.target.value}
+    });
+    setInputFields(newFields);
   };
 
   const submit = e => {
@@ -49,22 +47,23 @@ function App() {
               <div className="form-group col-sm-6">
                 <label htmlFor="firstName">First Name</label>
                 <input type="text" className="form-control" id="firstName" name="firstName" value={inputField.firstName}
-                  onChange={event => inputChange(index, event)}
+                       onChange={event => inputChange(index, event)}
                 />
               </div>
               <div className="form-group col-sm-4">
                 <label htmlFor="lastName">Last Name</label>
                 <input type="text" className="form-control" id="lastName" name="lastName"
-                  value={inputField.lastName}
-                  onChange={event => inputChange(index, event)}
+                       value={inputField.lastName}
+                       onChange={event => inputChange(index, event)}
                 />
               </div>
               <div className="form-group col-sm-2">
                 <button className="btn btn-link" type="button"
-                  onClick={() => removeFields(index)}>
-                  - </button>
+                        onClick={() => removeFields(index)}>
+                  -
+                </button>
                 <button className="btn btn-link" type="button"
-                  onClick={() => addFields()}>
+                        onClick={() => addFields()}>
                   +
                 </button>
               </div>
